2834062c9aec0649fdc89a5e5d667d974adffa4f
hoahayhoi <hoa.tmh2003@gmail.com>
2024-12-03 11:45:01 +0700

    Connect database mysql

---
+++ config/database.ts
@@ -0,0 +1,19 @@
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USERNAME,
+    process.env.DB_PASS,
+    {
+        host: process.env.DB_HOST,
+        dialect: "mysql"
+    }
+);
+
+sequelize.authenticate().then(() => {
+    console.log('Kết nối database thành công!');
+}).catch((error) => {
+    console.error('Kết nối database thất bại: ', error);
+});
+
+export default sequelize;
