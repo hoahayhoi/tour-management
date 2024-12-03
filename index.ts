f8158483ed2b15e8bd703ff2b350ac941b5e6214
hoahayhoi <hoa.tmh2003@gmail.com>
2024-12-03 11:31:32 +0700

    set up views folder

--- index.ts
+++ index.ts
@@ -1,10 +1,13 @@
 import express, { Express, Request, Response } from "express"
 
 const app: Express = express();
-const port: number = 3000; 
+const port: number = 3000;
 
-app.get("/", (req: Request, res: Response) => {
-    res.send("trang chủ");
+app.set('views', `${__dirname}/views`); // Tìm đến thư mục tên là views
+app.set('view engine', 'pug'); // template engine sử dụng: pug
+
+app.get("/tours", (req: Request, res: Response) => {
+    res.render("client/pages/tours/index");
 });
 
 app.listen(port, () => {
