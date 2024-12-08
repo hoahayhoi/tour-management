0c34be5286f4c211bde067d92588b23d11ded5f3
hoahayhoi <hoa.tmh2003@gmail.com>
2024-12-08 19:00:20 +0700

    create order detail when create order

--- controllers/client/order.controller.ts
+++ controllers/client/order.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 
 import Order from "../../models/order.model";
+import Tour from "../../models/tour.model";
+import OrderItem from "../../models/order-item.model";
 import { generateOrderCode } from "../../helpers/generate.helper";
 
 export const index = async (req: Request, res: Response) => {
@@ -19,7 +21,7 @@ export const index = async (req: Request, res: Response) => {
     const order = await Order.create(dataOrder);
     const orderId = order.dataValues.id;
     const code = generateOrderCode(orderId);
-    
+
     await Order.update({
         code: code
     }, {
@@ -27,6 +29,30 @@ export const index = async (req: Request, res: Response) => {
             id: orderId
         }
     });
+
+    // Lưu data vào bảng orders_items
+    for (const item of cart) {
+        const dataItem = {
+            orderId: orderId,
+            tourId: item.tourId,
+            quantity: item.quantity,
+        };
+
+        const tourInfo = await Tour.findOne({
+            where: {
+                id: item.tourId,
+                deleted: false,
+                status: "active"
+            },
+            raw: true
+        });
+
+        dataItem["price"] = tourInfo["price"];
+        dataItem["discount"] = tourInfo["discount"];
+        dataItem["timeStart"] = tourInfo["timeStart"];
+        await OrderItem.create(dataItem);
+    }
+
 
     res.json({
         code: "success",
